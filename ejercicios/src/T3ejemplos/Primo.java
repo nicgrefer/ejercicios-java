7f57b9b9accf1149f00751f01fe4d4e3708f10cc
nicgrefer <nicolas.jove.cubillo@gregoriofer.com>
2024-10-11 10:04:20 +0200

    Ejercicio primo
    acabado

--- ejercicios/src/T3ejemplos/Primo.java
+++ ejercicios/src/T3ejemplos/Primo.java
@@ -1,10 +1,31 @@
 package T3ejemplos;
 
+import java.util.Scanner;
+
 public class Primo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.print("Escriba cualquier numero: ");
+        Scanner sc= new Scanner(System.in);
+        int num=sc.nextInt();
+        int suma=0;
+        for(int i=1;i<=num;i++){
+            int aux=num%i;
+           
+            if(aux==0){
+                suma++;
+            }
+        }
+        if(suma<=2){
+            System.out.println("Es primo.");
+        }else{
+            System.out.println("No es primo.");
+        }
+
+		
+		
 	}
 
 }
