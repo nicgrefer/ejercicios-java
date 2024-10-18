b107e3d177af49eefdadde69db0e56875d318b68
nicgrefer <nicolas.jove.cubillo@gregoriofer.com>
2024-10-18 11:51:53 +0200

    Triangulo2

---
+++ ejercicios/src/T3ejemplos/Triangulo2.java
@@ -0,0 +1,16 @@
+package T3ejemplos;
+
+public class Triangulo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=4; i>=1; i--) {
+			for(int j=1; j<=i;j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
