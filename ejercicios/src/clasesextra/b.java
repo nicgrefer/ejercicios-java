6b548c931ad4fc4af2d1fe60711bf2e692d39320
nicgrefer <nicolas.jove.cubillo@gregoriofer.com>
2024-10-29 14:56:47 +0100

    ejercicos clase extra

--- ejercicios/src/clasesextra/b.java
+++ ejercicios/src/clasesextra/b.java
@@ -5,16 +5,28 @@ public class b {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
-		String datos[]= {"Javier Vergel","Jony Alonso","Mar Gonzalez","Ana Blanco"};
-		for (int i = 0; i < datos.length; i++) {
-			
-			String nombre=datos[i].substring(0, datos[i].indexOf(" "));
-			System.out.println(nombre);
-			
-			//
-			String apeyido=datos[i].substring(datos[i].indexOf(" ")+1);
-			System.out.println(apeyido);
+		  String b="_";
+	        int conta=0;
+	        int conta1=0;
+	        String datos[] = {"Javier Vergel", "Jorge Alonso", "Mar Gonzalez", "Ana Blanco"};
+	        for(int i = 0; i < datos.length; i++) {
+	            String nombre=datos[i].substring(0,datos[i].indexOf(" "));
+	            String apellido= datos[i].substring(datos[i].indexOf(" ")+1, datos[i].length());
+	            
+	            while(conta<(15-nombre.length())){
+	                System.out.print(b);
+	                conta++;
+	            }
+	            System.out.print(nombre);
+	            while(conta1<15-apellido.length()){
+	                
+	                System.out.print(b);
+	                conta1++;
+	            }
+	            System.out.println(apellido);
+	            conta=0;
+	            conta1=0;
+	        }
 		}
 	}
-}
+
