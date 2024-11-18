c321cdf987238ac4decf56468a7ca76f9ae04cc3
nicgrefer <nicolas.jove.cubillo@gregoriofer.com>
2024-11-18 12:18:22 +0100

    Acabado practica
    Falta comprobar funcionamientos y validaciones

---
+++ ejercicios/src/T6ejemplos/persona/Persona.java
@@ -0,0 +1,19 @@
+package T6ejemplos.persona;
+
+public class Persona {
+
+
+		//atributos --> estado
+		private String dni;
+		private String nombre;
+		private String apellido;
+		private int edad;
+		private boolean fumador;
+		private double altura; //en metros
+		
+		
+		//metodos --> comportamiento
+		
+	
+
+}
