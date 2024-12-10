23a2f092b824aceb358166d35b9adccc8fffc145
nicgrefer <nicolas.jove.cubillo@gregoriofer.com>
2024-12-10 13:36:40 +0100

    ejercicio empezado tele

---
+++ ejercicios/src/T6ejercicios/tele/AppTele.java
@@ -0,0 +1,16 @@
+package T6ejercicios.tele;
+
+public class AppTele {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Tele tele =new Tele("12Ar5", 24,50,10,false);
+		
+        System.out.println(tele.toString());
+		
+	}
+
+
+	
+}
