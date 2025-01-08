9896da3a3d66710a5769ac4132dc86e8720ac607
nicgrefer <nicolas.jove.cubillo@gregoriofer.com>
2025-01-08 13:57:20 +0100

    entornos

--- Ejemplo05_SinJavadok/src/main/Persona.java
+++ Ejemplo05_SinJavadok/src/main/Persona.java
@@ -1,5 +1,9 @@
 package main;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Persona.
+ */
 public class Persona {
 
 	/**
@@ -8,32 +12,74 @@ public class Persona {
 	 * @version 1.0
 	 * @since 07/06/2021
 	 * @autor coni <a href="https://www.linkedin.com/>Nombre</a>
-	 */
+	 */	
 	
 	private String nombre;
+	
+	/** The edad. */
 	private int edad;
 	
+	/**
+	 * Gets the nombre.
+	 *
+	 * @return the nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
+	
+	/**
+	 * Sets the nombre.
+	 *
+	 * @param nombre the new nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	/**
+	 * Gets the edad.
+	 *
+	 * @return the edad
+	 */
 	public int getEdad() {
 		return edad;
 	}
+	
+	/**
+	 * Sets the edad.
+	 *
+	 * @param edad the new edad
+	 */
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+	
+	/**
+	 * To string.
+	 *
+	 * @return the string
+	 */
 	@Override
 	public String toString() {
 		return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
 	}
+	
+	/**
+	 * Instantiates a new persona.
+	 *
+	 * @param nombre the nombre
+	 * @param edad the edad
+	 */
 	public Persona(String nombre, int edad) {
 		super();
 		this.nombre = nombre;
 		this.edad = edad;
 	}
+	
+	/**
+	 * Instantiates a new persona.
+	 */
 	public Persona() {
 		super();
 		// TODO Auto-generated constructor stub
---
+++ Ejemplo06_ConJavadokMeiben/src/main/java/Aplicación.java
@@ -0,0 +1,17 @@
+
+
+public class Aplicación {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		System.out.println("Hola mundo");
+		
+		Persona p =new Persona();
+		System.out.println(p);
+		
+		
+	}
+
+}
