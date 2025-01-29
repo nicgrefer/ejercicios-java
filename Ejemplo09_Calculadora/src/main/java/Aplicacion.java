abe1ff8e9c7be5166df81db4640713f9e5491026
nicgrefer <nicolas.jove.cubillo@gregoriofer.com>
2025-01-29 12:28:53 +0100

    Entormos

---
+++ Ejemplo09_Calculadora/src/main/java/Aplicacion.java
@@ -0,0 +1,16 @@
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		
+		Calculadora c1 = new Calculadora();
+		
+		System.out.println(c1.sumar(5,8));
+		System.out.println(c1.restar(5,8));
+		System.out.println(c1.producto(5,8));
+		System.out.println(c1.division(5,8));
+		
+	}
+	
+	
+}
---
+++ Ejemplo09_Calculadora/src/main/java/Calculadora.java
@@ -0,0 +1,29 @@
+
+public class Calculadora {
+
+
+	public int sumar(int num1,int num2) {
+		int resultadoSuma = num1 + num2;
+		return resultadoSuma;
+	}
+	
+	public int restar(int num1,int num2) {
+		int resultadoResta = num1 - num2;
+		return resultadoResta;
+	}
+	
+	public int producto(int num1,int num2) {
+		int resultadoProducto = num1 * num2;
+		return resultadoProducto;
+	}
+	
+	public double division(int num1,int num2) {
+		double resultadoDivision = num1/num2;
+		return resultadoDivision;
+	}
+	
+	
+	
+	
+	
+}
