f9fd1574df090d8734ad3919d7b14840ef471e30
nicgrefer <nicolas.jove.cubillo@gregoriofer.com>
2025-02-12 13:55:18 +0100

    Inicio ejercicio Calculadora Entornos

---
+++ Ejercicio02_Calculadora/src/main/java/APP.java
@@ -0,0 +1,20 @@
+
+public class APP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calculadora calc = new Calculadora();
+		
+		int n1=2;
+		int n2=2;
+		
+		int d1=1;
+		int d2=2;
+	
+		calc.multiplicar(n1,d1, n2,d2);
+		
+		
+	}
+
+}
---
+++ Ejercicio02_Calculadora/src/main/java/Calculadora.java
@@ -0,0 +1,13 @@
+
+public class Calculadora {
+
+	public void multiplicar(int n1, int d1, int n2, int d2) {
+		// TODO Auto-generated method stub
+		int n3= n1*n2;
+		int d3= d1*d2;
+		System.out.println(n3 + "/" +d3);
+	}
+
+	
+	
+}
