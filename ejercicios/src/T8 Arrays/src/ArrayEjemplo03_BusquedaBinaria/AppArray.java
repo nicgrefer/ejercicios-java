fcb22fc87aaeedeb531a35205d124096fb4d9ddf
nicgrefer <nicolas.jove.cubillo@gregoriofer.com>
2025-02-18 12:20:46 +0100

    Ejemplo Búsqueda binaria

--- ejercicios/src/T8 Arrays/src/ArrayEjemplo03_BusquedaBinaria/AppArray.java
+++ ejercicios/src/T8 Arrays/src/ArrayEjemplo03_BusquedaBinaria/AppArray.java
@@ -1,7 +1,8 @@
-package ArrayEjemplo_Ordenación;
+package ArrayEjemplo03_BusquedaBinaria;
 
-import ArrayEjemplo_Objetos.*;
+import ArrayEjemplo02_Ordenación.*;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class AppArray {
 
@@ -16,14 +17,28 @@ public class AppArray {
         Persona persona1 = new Persona("Pepe", "Perez", "11111111a", 20);
         Persona persona2 = new Persona("Maria", "Tierra", "22222222b", 15);
         Persona persona3 = new Persona("Marco", "Aurelio", "33333333c", 42);
+        Persona persona4 = new Persona("Luis", "Miguel", "44444444d", 18);
+        Persona persona5 = new Persona("Tio", "Pio", "55555555e", 32);
 
         // Asignarlas al array
         arrayPersonas[0] = persona1;
         arrayPersonas[1] = persona2;
         arrayPersonas[2] = persona3;
+        arrayPersonas[3] = persona4;
+        arrayPersonas[4] = persona5;
 
-        
-        Arrays.sort(arrayPersonas,0,3);
+
         System.out.println(Arrays.toString(arrayPersonas));        
+        
+         // Buscar una persona por apellidos usando el algoritmo binary search
+        Arrays.sort(arrayPersonas, Comparator.comparing(Persona::getDni));
+        Persona key = new Persona(null, null, "22222222b", null);
+        int resultado = Arrays.binarySearch(arrayPersonas, key, Comparator.comparing(Persona::getDni));
+
+        if (resultado >= 0) {
+            System.out.println("Persona encontrada: " + arrayPersonas[resultado]);
+        } else {
+            System.out.println("Persona no encontrada.");
+        }
     }
 }
--- ejercicios/src/T8 Arrays/src/ArrayEjemplo03_BusquedaBinaria/Persona.java
+++ ejercicios/src/T8 Arrays/src/ArrayEjemplo03_BusquedaBinaria/Persona.java
@@ -1,70 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ArrayEjemplo_Ordenación;
+package ArrayEjemplo03_BusquedaBinaria;
 
-/**
- *
- * @author jovcubni
- */
-public class Persona implements Comparable<Persona>{
-    
+public class Persona {
     private String nombre;
     private String apellidos;
     private String dni;
-    private int edad;
+    private Integer edad;
+
+    public Persona(String nombre, String apellidos, String dni, Integer edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.edad = edad;
+    }
 
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getApellidos() {
         return apellidos;
     }
 
-    public String getDni() {
-        return dni;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
- 
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
     }
 
     public void setDni(String dni) {
         this.dni = dni;
     }
 
-    public void setEdad(int edad) {
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
         this.edad = edad;
     }
 
-    public Persona(String nombre, String apellidos, String dni, int edad) {
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.dni = dni;
-        this.edad = edad;
-    }
-
-
-       @Override
-    public String toString() {
-        return "Persona{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", dni=" + dni + ", edad=" + edad + '}';
-    }
-// Criterio de comparacion por apellidos ascendente  (a->z)
     @Override
-    public int compareTo(Persona otra) {
-       return this.getApellidos().compareTo(otra.apellidos);
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", dni='" + dni + '\'' +
+                ", edad=" + edad +
+                '}';
     }
-    
 }
