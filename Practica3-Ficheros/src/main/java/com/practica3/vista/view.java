331921eb0ca064f2c2c40c1b0e47ed8ac22d00ef
nicjrerfer <nicolas.jove.cubillo@gregoriofer.com>
2025-05-18 16:41:30 +0200

    Creación de clase app para ejecutar

--- Practica3-Ficheros/src/main/java/com/practica3/vista/view.java
+++ Practica3-Ficheros/src/main/java/com/practica3/vista/view.java
@@ -288,6 +288,9 @@ public class view extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_BotonAñadirDatosActionPerformed
 
+    // Funciones para ordenar
+
+    
     private void idItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_idItemStateChanged
         // Ordenar por id
         if (empleados == null || empleados.isEmpty()) {
@@ -307,7 +310,7 @@ public class view extends javax.swing.JFrame {
     }//GEN-LAST:event_idItemStateChanged
 
     private void apellidoItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_apellidoItemStateChanged
-        // TODO add your handling code here:
+        // Ordenar por Apellido
         
         if (empleados == null || empleados.isEmpty()) {
             JOptionPane.showMessageDialog(this, "No hay empleados para ordenar.", "Info", JOptionPane.INFORMATION_MESSAGE);
@@ -326,7 +329,7 @@ public class view extends javax.swing.JFrame {
     }//GEN-LAST:event_apellidoItemStateChanged
 
     private void DepartamentoItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_DepartamentoItemStateChanged
-        // TODO add your handling code here:
+        // Ordenar por departamento
         
         if (empleados == null || empleados.isEmpty()) {
             JOptionPane.showMessageDialog(this, "No hay empleados para ordenar.", "Info", JOptionPane.INFORMATION_MESSAGE);
@@ -347,42 +350,6 @@ public class view extends javax.swing.JFrame {
     
     
     
-    // Funcion para ordenar
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(view.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(view.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(view.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(view.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new view().setVisible(true);
-            }
-        });
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BotonAñadirDatos;
     private javax.swing.JButton BotonBuscar;
